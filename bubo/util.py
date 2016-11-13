b0987248ac0061636cdfbcf005b2deaf1ca4c5d8
Dan Brown <jdanbrown@gmail.com>
2016-11-13 00:10:28 -0800

    Export bubo.mpl_backend_xee -> jdanbrown.mpl_backend_xee

--- bubo/util.py
+++ bubo/util.py
@@ -1,108 +1,16 @@
 from contextlib import contextmanager
-from copy import deepcopy
-import ggplot as gg
-import matplotlib as mpl
-import matplotlib.image
-import matplotlib.pyplot as plt
-import os
-import sys
 
-import bubo.mpl_backend_xee
+from jdanbrown.plot import *
+from jdanbrown.util import *
+from jdanbrown.util import singleton
+
 
 caffe_root = 'caffe-root'
 
-def shell(cmd):
-    print >>sys.stderr, 'shell: cmd[%s]' % cmd
-    status = os.system(cmd)
-    if status != 0:
-        raise Exception('Exit status[%s] from cmd[%s]' % (status, cmd))
-
-def singleton(cls):
-    return cls()
 
 def show_tuple_tight(xs):
     return '(%s)' % ','.join(map(str, xs))
 
-def puts(x):
-    print x
-    return x
-
-class gg_xtight(object):
-    def __init__(self, margin=0.05):
-        self.margin = margin
-    def __radd__(self, g):
-        g          = deepcopy(g)
-        xs         = g.data[g._aes['x']]
-        lims       = [xs.min(), xs.max()]
-        margin_abs = float(self.margin) * (lims[1] - lims[0])
-        g.xlimits  = [xs.min() - margin_abs, xs.max() + margin_abs]
-        return g
-
-class gg_ytight(object):
-    def __init__(self, margin=0.05):
-        self.margin = margin
-    def __radd__(self, g):
-        g          = deepcopy(g)
-        ys         = g.data[g._aes['y']]
-        lims       = [ys.min(), ys.max()]
-        margin_abs = float(self.margin) * (lims[1] - lims[0])
-        g.ylimits  = [ys.min() - margin_abs, ys.max() + margin_abs]
-        return g
-
-class gg_tight(object):
-    def __init__(self, margin=0.05):
-        self.margin = margin
-    def __radd__(self, g):
-        return g + gg_xtight(self.margin) + gg_ytight(self.margin)
-
-def gg_layer(*args):
-    'More uniform syntax than \ and + for many-line layer addition'
-    return reduce(lambda a,b: a + b, args)
-
-class gg_theme_keep_defaults_for(gg.theme_gray):
-    def __init__(self, *rcParams):
-        super(gg_theme_keep_defaults_for, self).__init__()
-        for x in rcParams:
-            del self._rcParams[x]
-
-def plot_gg(ggplot):
-    ggplot += gg_theme_keep_defaults_for('figure.figsize')
-    with bubo.mpl_backend_xee.basename_suffix(ggplot.title):
-        repr(ggplot) # (Over)optimized for repl/notebook usage (repr(ggplot) = ggplot.make(); plt.show())
-    #return ggplot # Don't return to avoid plotting a second time if repl/notebook
-
-def plot_img(data, basename_suffix=''):
-    with bubo.mpl_backend_xee.basename_suffix(basename_suffix):
-        return bubo.mpl_backend_xee.imsave_xee(data)
-
-def plot_img_via_imshow(data):
-    'Makes lots of distorted pixels, huge PITA, use imsave/plot_img instead'
-    (h,w) = data.shape[:2] # (h,w) | (h,w,3)
-    dpi   = 100
-    k     = 1 # Have to scale this up to ~4 to avoid distorted pixels
-    with tmp_rcParams({
-        'image.interpolation': 'nearest',
-        'figure.figsize':      puts((w/float(dpi)*k, h/float(dpi)*k)),
-        'savefig.dpi':         dpi,
-    }):
-        img = plt.imshow(data)
-        img.axes.get_xaxis().set_visible(False) # Don't create padding for axes
-        img.axes.get_yaxis().set_visible(False) # Don't create padding for axes
-        plt.axis('off')                         # Don't draw axes
-        plt.tight_layout(pad=0)                 # Don't add padding
-        plt.show()
-
-@contextmanager
-def tmp_rcParams(kw):
-    _save_mpl = mpl.RcParams(**mpl.rcParams)
-    _save_plt = mpl.RcParams(**plt.rcParams)
-    try:
-        mpl.rcParams.update(kw)
-        plt.rcParams.update(kw) # TODO WHY ARE THESE DIFFERENT
-        yield
-    finally:
-        mpl.rcParams = _save_mpl
-        plt.rcParams = _save_plt
 
 # Gross and horrible, but useful to help me understand how np.array shapes change as we transform them
 @singleton
@@ -120,7 +28,7 @@ class show_shapes:
             self.shapes.append(self._format_shape(data.shape))
             try:
                 yield
-                print '%s.show_shapes:%s' % (desc, self._show_compressed_shapes(1, self._compress_seq_pairs(self.shapes)))
+                print('%s.show_shapes:%s' % (desc, self._show_compressed_shapes(1, self._compress_seq_pairs(self.shapes))))
             finally:
                 self.shapes = None
 
@@ -137,8 +45,8 @@ class show_shapes:
         if isinstance(compressed_shapes, str):
             return compressed_shapes
         elif isinstance(compressed_shapes, tuple):
-            (n,x) = compressed_shapes
-            s     = self._show_compressed_shapes(level, x)
+            (n, x) = compressed_shapes
+            s      = self._show_compressed_shapes(level, x)
             return s if n == 1 else '%s%s%s' % (n, s, '  '*(level-1))
         else:
             return self._show_shapes(level, [self._show_compressed_shapes(level+1, x) for x in compressed_shapes])
