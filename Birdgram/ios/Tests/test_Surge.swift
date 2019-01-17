1fc939cd43d16e9d02d0b2717c5e89c61363b388
Dan Brown <jdanbrown@gmail.com>
2019-01-16 17:03:35 -0800

    mobile/ios/tests: Add test for Matrix.subscript

--- Birdgram/ios/Tests/test_Surge.swift
+++ Birdgram/ios/Tests/test_Surge.swift
@@ -42,3 +42,27 @@ test("Surge: det(X) doesn't crash on empty input") { name in
   testEqual(name, det(M(1,1,1)), 1)
   testEqual(name, det(M(0,0,0)), nil) // (Blas prints warning, but Surge handles return code)
 }
+
+test("Surge: subscript(rows: / columns: / rows:columns:)") { name in
+  let rows: [[Float]] = [
+    [ 0,  1,  2,  3,  4],
+    [ 5,  6,  7,  8,  9],
+    [10, 11, 12, 13, 14],
+  ]
+  let M = Matrix(rows)
+  let columns: [[Float]] = [
+    [ 0,  5, 10],
+    [ 1,  6, 11],
+    [ 2,  7, 12],
+    [ 3,  8, 13],
+    [ 4,  9, 14],
+  ]
+  testEqual(name, M[rows: 0..<3], Matrix(rows[0..<3]))
+  testEqual(name, M[rows: 1..<3], Matrix(rows[1..<3]))
+  testEqual(name, M[rows: 1..<2], Matrix(rows[1..<2]))
+  testEqual(name, M[rows: 1..<1], Matrix(rows[1..<1], rows: 0, columns: M.columns)) // 0xN not 0x0
+  testEqual(name, M[columns: 0..<5], Matrix(columns[0..<5]).T)
+  testEqual(name, M[columns: 0..<4], Matrix(columns[0..<4]).T)
+  testEqual(name, M[columns: 1..<2], Matrix(columns[1..<2]).T)
+  testEqual(name, M[columns: 2..<2], Matrix(columns[2..<2], rows: 0, columns: M.rows).T) // Nx0 not 0x0
+}
