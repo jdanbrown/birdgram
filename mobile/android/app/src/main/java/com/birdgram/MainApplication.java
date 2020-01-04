67137fb72a96aa2be000283788d6621c4c93543a
Dan Brown <jdanbrown@gmail.com>
2020-01-03 22:32:20 -0800

    mobile/ios: Replace react-native-sqlite-storage (deprecated, old sqlite<3.26) -> react-native-sqlite-plugin-legacy-support (supported, new sqlite>=3.26.x)

--- mobile/android/app/src/main/java/com/birdgram/MainApplication.java
+++ mobile/android/app/src/main/java/com/birdgram/MainApplication.java
@@ -3,6 +3,7 @@ package com.birdgram;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sqlc.SQLiteSupportPackage;
 import io.sentry.RNSentryPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SQLiteSupportPackage(),
             new RNSentryPackage(),
             new RNDeviceInfo(),
             new ReactNativeConfigPackage(),
